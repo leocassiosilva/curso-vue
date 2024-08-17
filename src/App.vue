71d58a994a9c16986a7168a1796fe1a1f996727f
leocassiosilva <leocassiosilva1234@gmail.com>
2024-08-17 20:55:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- Script -->
+<script setup>
+
+//importação dos componentes
+import Exemplo01 from './componentes/Exemplo01Interpolacao.vue'
+</script>
+
+<!--HTML-->
+<template>
+    <Exemplo01></Exemplo01>
+</template>
---
+++ src/componentes/Exemplo01Interpolacao.vue
@@ -0,0 +1,13 @@
+<!-- Script -->
+<script setup>
+let nome = 'Alice';
+let idade = 32;
+let numero1 = 20;
+let numero2 = 5;
+</script>
+
+<!-- Html -->
+<template>
+<p>{{ 'Olá ' + nome + 'você tem ' + idade + ' anos.'}}</p>
+<p>{{ numero1+numero2 }}</p>
+</template>
